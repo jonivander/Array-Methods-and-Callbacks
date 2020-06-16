c56c34e7213bd2e16fc019255dedcc51bd194f76
jonathan warner <j.m.warners@gmail.com>
2020-06-16 13:51:08 -0700

    Working on Tasks 1 and 2

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
-
+// (a) Home Team name for 2014 world cup final
+console.log(fifaData.filter((game) => game["Year"] === 2014 && game["Stage"] === "Final").map(game => game["Home Team Name"]) );
+// (b) Away Team name for 2014 world cup final
+console.log(fifaData.filter((game) => game["Year"] === 2014 && game["Stage"] === "Final").map(game => game["Away Team Name"]) );
+// (c) Home Team goals for 2014 world cup final
+console.log(fifaData.filter((game) => game["Year"] === 2014 && game["Stage"] === "Final").map(game => game["Home Team Goals"]) );
+// (d) Away Team goals for 2014 world cup final
+console.log(fifaData.filter((game) => game["Year"] === 2014 && game["Stage"] === "Final").map(game => game["Away Team Goals"]) );
+// (e) Winner of 2014 world cup final 
+console.log()
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
-};
-
+// function getFinals(data) {
+//     const data = fifaData.map(function(item){
+//         return item.stage === "Final";
+//     });
+// };
+// console.log(getFinals);
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
 function getYears(/* code here */) {
